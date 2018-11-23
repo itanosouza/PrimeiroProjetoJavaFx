b4ed7175ae62045789029b31fc55b22d58151023
Itano Souza <itano.souza@gmail.com>
2018-11-23 16:49:00 -0300

    Criando elementos para paisagem musical

--- Documents/NetBeansProjects/HelloWordJavaFx/src/HelloWordJavaFx.java
+++ Documents/NetBeansProjects/HelloWordJavaFx/src/HelloWordJavaFx.java
@@ -1,7 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
@@ -13,12 +14,14 @@ public class HelloWordJavaFx extends Application{
     @Override
     public void start(Stage palco){
         //personagem
-        Label texto = new Label ("Ultimo dia de Aula");
+        Rectangle ceu = new Rectangle (900, 400);
+        Rectangle mar = new Rectangle(900, 450);
+        Circle sol = new Circle(90);
         //historia
         StackPane historia = new StackPane();
-        historia.getChildren().add(texto);
+        historia.getChildren().addAll(ceu, sol, mar);
         //cena
-        Scene cena = new Scene(historia, 300, 300);
+        Scene cena = new Scene(historia, 900, 900);
         //colocando a cena no palco
         palco.setScene(cena);
         palco.show();
