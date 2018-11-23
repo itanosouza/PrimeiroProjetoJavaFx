c1df0d4aec0c6e5a1071a7e39e81b468728e8e81
Itano Souza <itano.souza@gmail.com>
2018-11-23 17:09:13 -0300

    Adicionando cores e reposionando elemento

--- Documents/NetBeansProjects/HelloWordJavaFx/src/HelloWordJavaFx.java
+++ Documents/NetBeansProjects/HelloWordJavaFx/src/HelloWordJavaFx.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -14,9 +15,13 @@ public class HelloWordJavaFx extends Application{
     @Override
     public void start(Stage palco){
         //personagem
-        Rectangle ceu = new Rectangle (900, 400);
-        Rectangle mar = new Rectangle(900, 450);
-        Circle sol = new Circle(90);
+        Rectangle ceu = new Rectangle (900, 450, Color.DARKORANGE);
+        Rectangle mar = new Rectangle(900, 450, Color.DODGERBLUE);
+        Circle sol = new Circle(95, Color.YELLOW);
+        //posicionamento
+        ceu.setTranslateY(-200);
+        mar.setTranslateY(200);
+        sol.setTranslateY(40);
         //historia
         StackPane historia = new StackPane();
         historia.getChildren().addAll(ceu, sol, mar);
@@ -24,6 +29,7 @@ public class HelloWordJavaFx extends Application{
         Scene cena = new Scene(historia, 900, 900);
         //colocando a cena no palco
         palco.setScene(cena);
+        palco.setTitle("Patamares");
         palco.show();
     
     }
